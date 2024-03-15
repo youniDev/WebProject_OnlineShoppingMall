428da2dca468dd12e83d2d1c925059fec1d5abda
yj2980 <gum0945@naver.com>
2024-03-15 16:45:07 +0900

    init server

---
+++ server/src/main/java/com/example/onlineStore/OnlineStoreApplication.java
@@ -0,0 +1,12 @@
+package com.example.onlineStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+}
