3d0a5261d66f997cf8479d12c8e140a1cb765734
yj2980 <gum0945@naver.com>
2024-03-15 22:39:36 +0900

    feat: 게시물 관련 기능 추가
    - 게시물 등록, 수정, 조회, 삭제 기능 추가
    - 댓글 및 대댓글 등록, 삭제, 조회 기능 추가

---
+++ app/shoppingmall-app/src/component/layout/Comment.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { Row, Form, Button, Col } from "react-bootstrap";
+
+import { ERROR } from "../../assets/js/Constants";
+import { addComment } from "../../api/BoardAPI";
+
+const Comment = ({postId, userId, parentCommentId, onCloseComment}) => {
+    const [comment, setComment] = useState({
+        content: '',
+        image: '',
+    });
+
+    const handleCommentChange = (e) => {
+        const { name, value } = e.target;
+        setComment(prevPost => ({
+          ...prevPost,
+          [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        try {
+            postComment();
+            onCloseComment();
+        } catch (error) {
+            console.error(ERROR.FAIL_COMMUNICATION, error);
+        }
+    }
+
+    const postComment = async() => {
+        try {
+            await addComment(postId, userId, parentCommentId, comment);
+        } catch (error) {
+            console.error(ERROR.FAIL_COMMUNICATION, error);
+        }
+    }
+
+    return (
+        <Row>
+            <div className="col-12">
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="commentContent">
+                        <Form.Control
+                            as="textarea"
+                            name="content"
+                            rows={3}
+                            placeholder="댓글을 입력하세요"
+                            value={comment.content}
+                            onChange={handleCommentChange}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        등록
+                    </Button>
+                </Form>
+            </div>
+        </Row>
+    );
+};
+
+export default Comment;
